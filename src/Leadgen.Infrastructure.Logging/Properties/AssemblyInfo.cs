ea92f872bed6f6ea9a9b265871497499cef2962c
Cristian Leon <Cristian.Leon@cignium.com>
2018-12-04 16:50:25 -0500

    Updated AssemblyInfo and Nuspec for package

--- src/Leadgen.Infrastructure.Logging/Properties/AssemblyInfo.cs
+++ src/Leadgen.Infrastructure.Logging/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Leadgen.Infrastructure.Logging")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Leadgen Infrastructure - Logging")]
+[assembly: AssemblyDescription("Generic logging library for Leadgen Sites.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tranzact Digital Team")]
 [assembly: AssemblyProduct("Leadgen.Infrastructure.Logging")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
